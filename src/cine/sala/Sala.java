fb819f5fe831fac2ad7d1a6ee85f3c7ff27670e1
Andres Hoyos <andres.hoyos18@eia.edu.co>
2021-03-06 19:23:44 -0500

    Agrego la Clase Sala.java
    Metodos:
      public boolean asignarAsiento(fila:int, columna:int): Regresa True si se pudo asignar el asiento, False de lo contrario.
      public boolean reservarAsiento(fila:int, columna:int): regresa TRUE si se pudo reservar el asiento, FALSE de lo contrario.
      public boolean liberarAsiento(fila:int, columna:int): libera el asiento.
      public void limpiarSala():Hace que todos los asientos esten disponibles.

--- src/cine/sala/Sala.java
+++ src/cine/sala/Sala.java
@@ -1,5 +1,134 @@
 package cine.sala;
 
+import cine.pelicula.Pelicula;
+import cine.sala.asiento.Asiento;
+import cine.sala.asiento.EstadoAsiento;
+import cine.sala.asiento.TipoAsiento;
+import exceptions.InvalidArgumentE;
+
+import java.util.Arrays;
+
 public class Sala {
 
+    private final Asiento[][] asientos;
+    private Pelicula pelicula;
+    private TipoSala tipoSala;
+
+    public Sala(Pelicula pelicula, TipoSala tipoSala, int filas, int columnas) {
+        this.pelicula = pelicula;
+        this.tipoSala = tipoSala;
+        this.asientos = new Asiento[filas][columnas];
+
+        fillAsientos();
+
+    }
+
+    public boolean asignarAsiento(int fila, int columna) throws InvalidArgumentE {
+        if (fila >= asientos.length || fila < 0) {
+            throw new InvalidArgumentE("LA FILA NO ES VALIDA");
+        } else if (columna >= asientos[0].length || columna < 0) {
+            throw new InvalidArgumentE("LA COLUMNA NO ES VALIDA");
+        }
+
+        if (asientos[fila][columna].getEstadoAsiento() == EstadoAsiento.DISPONIBLE) {
+            asientos[fila][columna].setEstadoAsiento(EstadoAsiento.OCUPADO);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean reservarAsiento(int fila, int columna) throws InvalidArgumentE {
+        if (fila >= asientos.length || fila < 0) {
+            throw new InvalidArgumentE("LA FILA NO ES VALIDA");
+        } else if (columna >= asientos[0].length || columna < 0) {
+            throw new InvalidArgumentE("LA COLUMNA NO ES VALIDA");
+        }
+
+        if (asientos[fila][columna].getEstadoAsiento() == EstadoAsiento.DISPONIBLE) {
+            asientos[fila][columna].setEstadoAsiento(EstadoAsiento.RESERVADO);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public void liberarAsiento(int fila, int columna) throws InvalidArgumentE {
+        if (fila >= asientos.length || fila < 0) {
+            throw new InvalidArgumentE("LA FILA NO ES VALIDA");
+        } else if (columna >= asientos[0].length || columna < 0) {
+            throw new InvalidArgumentE("LA COLUMNA NO ES VALIDA");
+        }
+
+        asientos[fila][columna].setEstadoAsiento(EstadoAsiento.DISPONIBLE);
+
+    }
+
+    private void fillAsientos() {
+        //00% - 40% General
+        //40% - 60% Preferencial
+        //60% - 80% Platinum
+        //80% - 100% Gold
+        final int filas = asientos.length;
+        final int general = (int) Math.round(filas * 0.4);
+        final int preferencial = (int) Math.round(filas * 0.6);
+        final int platinum = (int) Math.round(filas * 0.8);
+//        final int gold = (int) Math.round(filas);
+
+        for (int i = 0; i < asientos.length; i++) {
+            if (i < general) {
+                Arrays.fill(asientos[i], Asiento.crearAsientoDisponible(TipoAsiento.GENERAL));
+            } else if (general <= i && i < preferencial) {
+                // i < preferencial
+                Arrays.fill(asientos[i], Asiento.crearAsientoDisponible(TipoAsiento.PREFERENCIAl));
+            } else if (preferencial <= i && i < platinum) {
+                // i < platinum
+                Arrays.fill(asientos[i], Asiento.crearAsientoDisponible(TipoAsiento.PLATINUM));
+            } else {
+                Arrays.fill(asientos[i], Asiento.crearAsientoDisponible(TipoAsiento.GOLD));
+            }
+        }
+    }
+
+    public String mostrarSala() {
+        StringBuilder sb = new StringBuilder();
+
+        for (Asiento[] asiento : asientos) {
+            for (Asiento value : asiento) {
+                sb.append("[").append(value.getEstadoAsiento()).append("]");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public void limpiarSala() {
+        for (int i = 0; i < asientos.length; i++) {
+            for (int j = 0; j < asientos[i].length; j++) {
+                try {
+                    liberarAsiento(i, j);
+                } catch (InvalidArgumentE invalidArgumentE) {
+                    invalidArgumentE.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+
+    public TipoSala getTipoSala() {
+        return tipoSala;
+    }
+
+    public void setPelicula(Pelicula pelicula) {
+        limpiarSala();
+        this.pelicula = pelicula;
+    }
+
+    public void setTipoSala(TipoSala tipoSala) {
+        this.tipoSala = tipoSala;
+    }
 }
